db015975af8ba2c76230c23d9ef8d646f5cd73d6
Binnukrishna <binnukrishna5233@gmail.com>
2025-04-17 21:44:19 +0530

    update : gitignore udpated

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,17 @@
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+      <h1>Login</h1>
+      <div className="flex flex-col items-start justify-start gap-2">
+        <label htmlFor="email">Email</label>
+        <input className="border" type="email" id="email" name="email" />
+      </div>
+      <div className="flex flex-col items-start justify-start gap-2">
+        <label htmlFor="password">Password</label>
+        <input className="border" type="password" id="password" name="password" />
+      </div>
+    </>
   )
 }
 
