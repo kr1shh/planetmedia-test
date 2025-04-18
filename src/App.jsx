9d1033345b002c2b678aa7c621499ab44cf9c534
Binnukrishna <binnukrishna5233@gmail.com>
2025-04-18 21:51:33 +0530

    update : dashboard pages udpated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {
   Route,
   createRoutesFromElements,
 } from "react-router-dom";
-import { AdsDetails, Dashboard, Home, Login, Register } from "./pages";
+import { AdsDetails, Dashboard, Home, Login, Register, MyAccount, Profile, PostAd } from "./pages";
 import { Toaster } from "react-hot-toast";
 import Layout from "./Layout";
 
@@ -16,7 +16,11 @@ const App = () => {
           <Route path="" element={<Home />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
-          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="dashboard" element={<Dashboard />}>
+            <Route path="/my-account" element={<MyAccount />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/post-ad" element={<PostAd />} />
+          </Route>
           <Route path="ads/:id" element={<AdsDetails />} />
         </Route>
         <Route path="*" element={<div>Not Found</div>} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,8 @@ import Dashboard from "./dashboard/Dashboard";
 import Home from "./home/Home";
 import Login from "./login/Login";
 import Register from "./register/Register";
+import MyAccount from "./myAccount/MyAccount";
+import Profile from "./profile/Profile";
+import PostAd from "./postAd/PostAd";
 
-export { AdsDetails, Dashboard, Home, Login, Register };
+export { AdsDetails, Dashboard, Home, Login, Register, MyAccount, Profile, PostAd };
