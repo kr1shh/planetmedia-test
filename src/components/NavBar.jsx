f4e21ddf9cc58c17cb7f230a66a4944eaa4d8967
Binnukrishna <binnukrishna5233@gmail.com>
2025-04-20 09:53:21 +0530

    updte : logo converted to root link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
-import { useNavigate } from "react-router"
+import { Link, useNavigate } from "react-router"
 import navLogo from "../assets/logoForWhite.png"
 import { useEffect, useState } from "react"
+
 
 
 const NavBar = () => {
@@ -17,7 +18,9 @@ const NavBar = () => {
     <>
         <nav className="flex justify-between items-center py-4 px-4 bg-white drop-shadow-xl">
             <div className="container mx-auto flex items-center justify-between">
-                <img src={navLogo} alt="logo" className="w-32"/>
+                <Link to="/">
+                    <img src={navLogo} alt="logo" className="w-32"/>
+                </Link>
                 <div className="flex gap-3">
                     <button className="border border-gray-200 rounded-[50px] px-6 py-2 cursor-pointer" onClick={()=>navigate("/login")}>
                         {userName ? `${userName}` : "Login"}
